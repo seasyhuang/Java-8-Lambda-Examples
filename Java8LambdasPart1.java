b3fb5b78bf985e81b67c501f77cf4e21d103de8b
Seasy <seasy.huang@gmail.com>
2018-01-30 10:59:17 -0500

    working on first set of exercises

--- Java8LambdasPart1.java
+++ Java8LambdasPart1.java
@@ -7,21 +7,35 @@ public class Java8LambdasPart1 {
 	
 	
 	public static void main(String[] args) {
+		
+//		http://www.java-programming.info/tutorial/pdf/java/13-Java-8-Lambda-Expressions-Part-1.pdf
 		String s1 = "hello world";
-		String s2 = "world hello";
-		String[] testStrings = new String[] {s1, s2};
+		String s2 = "world hello world";
+		String s3 = "testing string";
+		String s4 = "test";
+		String s5 = "lambdas";
+		String[] testStrings = new String[] {s1, s2, s3, s4, s5};
+		String[] lambdaTestStrings = new String[] {s1, s2, s3, s4, s5};
 		
-		// java 7 way
-		
+		// Array sorted by length - java 7
 		Arrays.sort(testStrings, new Comparator<String>() {
 			@Override
 			public int compare(String s1, String s2) {
 				return (s1.length() - s2.length());
 			}
 		});
+		System.out.println(Arrays.toString(testStrings));
 		
+		// Array sorted by length - java 8
+		System.out.println("Sorted by length");
+		Arrays.sort(lambdaTestStrings, (ls1, ls2) -> ls1.length() - ls2.length());
+		System.out.println(Arrays.toString(lambdaTestStrings));
+		
+		
+		// Array sorted by reverse length - java 8
+		System.out.println("Sorted by reverse length");
+		Arrays.sort(lambdaTestStrings, (ls1, ls2) -> ls2.length() - ls1.length());
+		System.out.println(Arrays.toString(lambdaTestStrings));
 		
 	}
-	
-
 }
